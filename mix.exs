5cac6890584019b009ca8d647da080592894ca6c
Scott Moynes <scott.moynes@gmail.com>
2016-10-06 12:15:50 -0700

    Add dependencies: plug, cowboy

--- mix.exs
+++ mix.exs
@@ -7,27 +7,18 @@ defmodule PlugLoadBalancer.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(Mix.env)]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
      mod: {PlugLoadBalancer, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    []
+  defp deps(_env) do
+    [
+      {:cowboy, "~> 1.0"},
+      {:plug, "~> 1.2"},
+    ]
   end
 end
