4085ceb465c3dbb93d941fefc43e59a36e274571
Scott Moynes <scott.moynes@gmail.com>
2016-10-29 12:04:20 -0700

    For development, proxy to a local port.

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,6 @@
 use Mix.Config
 
 config :ballast, routes: [
-   [host: "example.org", plug: {Ballast.Plug.Proxy, [origin: "httpbin.org"]}]
+  [host: "example.org", plug: {Ballast.Plug.Proxy, [origin: "localhost:4000"]}],
+  [host: "httpbin.org", plug: {Ballast.Plug.Proxy, [origin: "httpbin.org"]}]
 ]
