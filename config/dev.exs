956737db4f75d25a3caffe919fa34b27a935d620
Scott Moynes <scott.moynes@gmail.com>
2017-02-13 22:55:33 -0800

    Fix dev configuration.

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,6 @@
 use Mix.Config
 
 config :ballast, routes: [
-  [path: "/debug/get", prefix: "/debug/prefix", plug: {Ballast.Plug.Proxy, [origin: "localhost:4000"]}],
-  [host: "example.org", plug: {Ballast.Plug.Proxy, [origin: "localhost:4000"]}],
   [host: "httpbin.org", plug: {Ballast.Plug.Proxy, [origin: "httpbin.org"]}],
+  [path: "/debug", prefix: "/debug", plug: {Ballast.Plug.Proxy, [origin: "localhost:4000"]}],
 ]
