cd88db91facb08a87988325f4564bcb03a43694e
Scott Moynes <scott.moynes@gmail.com>
2017-02-16 21:51:42 -0800

    Configure logging during tests.

--- config/test.exs
+++ config/test.exs
@@ -4,3 +4,6 @@ config :ballast, [routes: [],
                   listener: Test.ProxyEndpoint,
                   proxy_port: 8888,
                   api_port: 5555]
+
+config :logger,
+  compile_time_purge_level: :debug
